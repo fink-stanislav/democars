8e5e3898f30d29c09fc99dd145937b8af72a227a
Fink <ravenhollm@gmail.com>
2011-02-10 20:27:19 +0200

    plain datamodel for datascroller added.

--- src/main/java/com/exadel/democars/beans/CarsCollectionBean.java
+++ src/main/java/com/exadel/democars/beans/CarsCollectionBean.java
@@ -4,14 +4,16 @@ import com.exadel.democars.application.DataManager;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import java.io.FileNotFoundException;
+import javax.faces.model.ArrayDataModel;
+import javax.faces.model.DataModel;
 import java.io.Serializable;
 
 @ManagedBean(name = "carsCollectionBean")
 @RequestScoped
 public class CarsCollectionBean implements Serializable {
+    private DataModel<Car> model = new ArrayDataModel<Car>(DataManager.getCarList());
 
-    public Car[] getCars() {
-        return DataManager.getCarList();
+    public DataModel<Car> getCars() {
+        return model;
     }
 }
